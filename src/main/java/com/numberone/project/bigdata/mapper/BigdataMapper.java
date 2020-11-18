1bd64a6e3ddb6546b91aec699cf856741ef07810
temptation <along__song@163.com>
2020-11-18 17:17:01 +0800

    用能汇总+用能占比+用能排名后台编写

--- src/main/java/com/numberone/project/bigdata/mapper/BigdataMapper.java
+++ src/main/java/com/numberone/project/bigdata/mapper/BigdataMapper.java
@@ -1,6 +1,8 @@
 package com.numberone.project.bigdata.mapper;
 
+import com.numberone.project.bigdata.domain.AreaRatio;
 import com.numberone.project.bigdata.domain.Zsum;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -13,4 +15,8 @@ import java.util.List;
 public interface BigdataMapper {
 
     List<Zsum> zsum();
+
+    Zsum areagross(@Param("largeareaname") String largeareaname);
+
+    List<AreaRatio> arearatio(@Param("table")String table, @Param("largeareaname") String largeareaname);
 }
